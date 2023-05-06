e5bdbfc71632e9a447b1c6504d691cb8df984ecc
seancraven <seanpietercraven@gmail.com>
2023-05-06 11:06:04 +0100

    generative model worktree

---
+++ src/client.rs
@@ -0,0 +1,64 @@
+// Current state, can send and recive messages from the server.
+//
+use std::{
+    io::{self, Write},
+    net::{TcpListener, TcpStream},
+};
+
+#[derive(Debug, Clone)]
+pub struct IPV4 {
+    address: String,
+}
+impl IPV4 {
+    fn address(&self, port: String) -> String {
+        format!("{}:{}", self.address, port)
+    }
+    fn new(address: String) -> IPV4 {
+        IPV4 { address }
+    }
+}
+pub struct Client {
+    host_ip: IPV4,
+    port: String,
+}
+impl Client {
+    pub fn new(host_ip: IPV4, port: String) -> Client {
+        Client { host_ip, port }
+    }
+    pub fn address(&self) -> String {
+        self.host_ip.address(self.port.clone())
+    }
+    /// Client starts, forms a connection and then returns an iterator over the response.
+    pub fn start_gen(&self) -> Result<TcpStream, io::Error> {
+        let address = self.address();
+        println!("Connecting to {}", address);
+        let mut stream = TcpStream::connect(&address)?;
+        stream.write_all("start".as_bytes())?;
+        return Ok(stream);
+    }
+}
+
+#[cfg(test)]
+mod client_test {
+    use super::*;
+    use dotenv::dotenv;
+    use std::{
+        env::var,
+        io::{BufRead, BufReader},
+    };
+    #[test]
+    fn server_test() {
+        dotenv().ok();
+        let ip = IPV4::new(var("IPV4").expect("Can't find .evn variable IPV4"));
+        let port = var("PORT").expect("Can't find .evn variable IPV4");
+        let client = Client::new(ip, port);
+        let stream = client.start_gen().expect("Failed to connect to host");
+        let reader = BufReader::new(stream);
+        reader
+            .lines()
+            .map(|line| line.expect("Failed to read line"))
+            .for_each(|line| {
+                println!("{}", line);
+            });
+    }
+}
